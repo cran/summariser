5b583247f1a974a9932d399bdf655b545dacde6f
Conor Neilson <condwanaland@gmail.com>
2017-03-23 12:10:18 +0000

    version 0.1.0

---
+++ R/summariser.R
@@ -0,0 +1,22 @@
+#' summariser: an R package for easily calculating basic summary statistics
+#'
+#' The summariser package provides two important functions: \code{summary_stat#' s} and \code{summary_plot}
+#'
+#' @section summary_stats functions
+#' \code{summary_stats} is called on a data frame to collect basic statistics.
+#' A call to summary_stats will produce mean, standard error, standard
+#' deviation, count, and confidence intervals for the data frame. Any
+#' number of grouping variables can be specified (as in dplyr \code{\link{grou#' p_by)}}
+#'
+#'
+#' @section summary_plot function
+#'\code{summary_plot} takes the output of \code{summary_stats} to produce a
+#' plot of group means and error bars
+#'
+#'
+#'
+#' @docType package
+#' @name summariser
+#'
+#'
+NULL
